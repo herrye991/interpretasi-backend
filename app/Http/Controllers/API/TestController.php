5359193e93c65547a721c918a900fe17fb5b1c9b
herrye991 <42521692+herrye991@users.noreply.github.com>
2023-01-25 15:27:29 +0000

    modified:   app/Http/Controllers/API/TestController.php

--- app/Http/Controllers/API/TestController.php
+++ app/Http/Controllers/API/TestController.php
@@ -9,13 +9,6 @@ class TestController extends Controller
 {
     public function index()
     {
-        $quill_json = '[{"insert":"Heading"},{"insert":"\\n","attributes":{"header":1}},{"insert":"bold","attributes":{"bold":true}},{"insert":"\\n"},{"insert":"bold and italic","attributes":{"bold":true,"italic":true}},{"insert":"\\nsome code"},{"insert":"\\n","attributes":{"code-block":true}},{"insert":"A quote"},{"insert":"\\n","attributes":{"blockquote":true}},{"insert":"ordered list"},{"insert":"\\n","attributes":{"list":"ordered"}},{"insert":"unordered list"},{"insert":"\\n","attributes":{"list":"bullet"}},{"insert":"link","attributes":{"link":"pub.dev/packages/quill_markdown"}},{"insert":"\\n"}]';
-        $quill_json = json_decode($quill_json);
-        $parser = new \DBlackborough\Quill\Parser\Html();
-        $renderer = new \DBlackborough\Quill\Renderer\Html();
-
-        $parser->load($quill_json)->parse();
-
-        echo $renderer->load($parser->deltas())->render();
+        //
     }
 }
