1e0762f463e45dbf3acd18306842a08784c84cd7
Herry Mandala <herrymandala@linuxmail.org>
2023-01-29 23:07:37 +0700

    Add test email

---
+++ resources/views/emails/verify.blade.php
@@ -0,0 +1,45 @@
+<center>
+    <table style="border-spacing:0;border-collapse:collapse;font-family:proxima-nova,'helvetica neue',helvetica,arial,geneva,sans-serif;width:100%!important;height:100%!important;color:#4c4c4c;font-size:15px;line-height:150%;background:#ffffff;margin:0;padding:0;border:0">
+        <tbody>
+            <tr style="vertical-align:top;padding:0">
+                <td align="center" valign="top" style="vertical-align:top;padding:0">
+                    <table style="border-spacing:0;border-collapse:collapse;font-family:proxima-nova,'helvetica neue',helvetica,arial,geneva,sans-serif;width:600px;color:#4c4c4c;font-size:15px;line-height:150%;background:#ffffff;margin:40px 0;padding:0;border:0">
+                        <tbody>
+                            <tr style="vertical-align:top;padding:0">
+                                <td align="center" valign="top" style="vertical-align:top;padding:0 40px">
+                                    <table style="border-spacing:0;border-collapse:collapse;font-family:proxima-nova,'helvetica neue',helvetica,arial,geneva,sans-serif;width:100%;background:#ffffff;margin:0;padding:0;border:0">
+                                        <tbody>
+                                            <tr style="vertical-align:top;padding:0">
+                                                <td style="vertical-align:top;text-align:left;padding:0" align="left" valign="top">
+                                                    <h1 style="color:#6e5baa;display:block;font-family:hybrea,proxima-nova,'helvetica neue',helvetica,arial,geneva,sans-serif;font-size:32px;font-weight:200;text-align:left;margin:0 0 40px" align="left">{{ $details['title'] }}</h1>
+                                                    <p style="margin:20px 0">{{ $details['body'][0] }}</p>
+                                                    <p style="margin:20px 0"><a href="{{ $details['body'][1] }}" style="color:#6e5baa" target="_blank" data-saferedirecturl="{{ $details['body'][1] }}">{{ $details['body'][1] }}</a></p>
+                                                    <p style="margin:20px 0">{{ $details['body'][2] }}</p>
+                                                    <p style="margin:20px 0">Interpretasi ID Team<br><a href="https://interpretasi.id" style="color:#6e5baa" target="_blank" data-saferedirecturl="https://interpretasi.id">https://interpretasi.id</a></p>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr style="vertical-align:top;padding:0">
+                <td align="center" valign="top" style="vertical-align:top;padding:0 40px">
+                    <table style="border-spacing:0;border-collapse:collapse;font-family:proxima-nova,'helvetica neue',helvetica,arial,geneva,sans-serif;width:100%;border-top-style:solid;border-top-color:#ebeaef;color:#999999;font-size:12px;background:#ffffff;margin:0;padding:0;border-width:1px 0 0">
+                        <tbody>
+                            <tr style="vertical-align:top;padding:0">
+                                <td valign="top" style="vertical-align:top;text-align:left;padding:0" align="left">
+                                    <p style="margin:20px 0">Interpretasi ID adalah web baca berita dan artikel</p>
+                                    <p style="margin:20px 0">Untuk mengetahui lebih lanjut tentang Interpretasi ID, Lihat developer center: <a href="https://interpretasi.id/about" style="color:#666666" target="_blank" data-saferedirecturl="https://interpretasi.id/abount">https://interpretasi.id/abount</a></p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</center>
