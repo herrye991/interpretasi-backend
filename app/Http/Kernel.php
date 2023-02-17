e24c4f16102835eec6265bed24ae601de891fb51
Herry Mandala <herrymandala@linuxmail.org>
2023-02-18 02:11:28 +0700

    throttle 300 request / minutes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:60,1',
+            'throttle:300,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
